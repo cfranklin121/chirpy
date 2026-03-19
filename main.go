7fa59ce6adaf9db0351472d8beb3f5198aa9d835
cfranklin121 <courtenayfranklin@gmail.com>
2026-03-18 20:27:09 -0700

    Minor cleanup

--- main.go
+++ main.go
@@ -24,12 +24,12 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf(`
-		<html>
-			<body>
-				<h1>Welcome, Chirpy Admin</h1>
-				<p>Chirpy has been visited %d times!</p>
-			</body>
-			</html>
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
 		`, cfg.fileserverHits.Load())))
 }
 
